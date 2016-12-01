1b5b804e426ca06677ace7af55d1c951a03a7457
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-12-01 16:14:14 -0700

    set up boilerplate, set up search bar as class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBVLXNNekLd7NZgtQ0I0632e1ujNmpHAJs'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. Should produce html
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+
+}
+
+// Take this component's generated html and put it on the page (in the DOM).
+ReactDOM.render(<App />, document.querySelector('.container'))
